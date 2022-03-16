02108055555637623ce2c53458705e2b2f3813b0
Jitendra Makvana <Jitumakvana1995@gmail.com>
2022-03-16 13:00:53 +0530

    Phase 1 Project LockedMe : Added getAllFiles() and Displaying all files to user.

--- src/com/simplilearn/phase1/finalproject/FileOperation.java
+++ src/com/simplilearn/phase1/finalproject/FileOperation.java
@@ -1,10 +1,32 @@
 package com.simplilearn.phase1.finalproject;
 
+import java.io.File;
+
 public class FileOperation {
 
+	//Declaring class variable
+	static final String projectFilesPath = "C:\\Users\\Jits\\Desktop\\Phase1_FinalProject_LockedMe\\Repository"; 
+	
 	public static void getAllFiles() {
-		// TODO Auto-generated method stub
-		
+
+		//Declaring Vairable and Instantiate Object 
+		File folder = new File(projectFilesPath);
+		String[] listOfFiles = folder.list();
+				
+		//Checking folder and files status
+		if(listOfFiles.length>0){
+			
+			//Printing all files
+			System.out.println("FILES LISTED BELOW :");
+			System.out.println("====================\n");
+			
+			for(String file : listOfFiles){
+				System.out.println(file);
+			}
+			
+		}else{
+			System.out.println("The folder is empty.");
+		}
 	}
 
 	public static void createFile() {
