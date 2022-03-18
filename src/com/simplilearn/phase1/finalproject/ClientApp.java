54b906f191fe4e27f95073a1caaf9bfc8b485f98
Jitendra Makvana <Jitumakvana1995@gmail.com>
2022-03-18 22:44:07 +0530

    Phase 1 Project LockedMe : ClientApp.java -- Handle exception by using exception handling concept. FileOperation.java -- Implements method including createFile(), deleteFile(), searchFile(), checkFileExist(String fileName) and checkFileExtension(String fileName). Also added documents comments

--- src/com/simplilearn/phase1/finalproject/ClientApp.java
+++ src/com/simplilearn/phase1/finalproject/ClientApp.java
@@ -11,38 +11,44 @@ public class ClientApp {
 		int option = 0;
 		
 		do{
-			//Display main menu
-			DisplayMenu.displayMainMenu();
+			try{
+				//Display main menu
+				DisplayMenu.displayMainMenu();
+				
+				//getting user input
+				System.out.print("Please enter your choice : ");
+				option = sc.nextInt();
+				
+				//Performing user selected operation 
+				switch(option){
+				
+				case 1:
+					FileOperation.getAllFiles();
+					break;
+				case 2:
+					FileOperation.createFile();
+					break;
+				case 3:
+					FileOperation.deleteFile();
+					break;
+				case 4:
+					FileOperation.searchFile();
+					break;
+				case 5:
+					System.out.println("Application closed successfully.");
+					System.exit(0);
+					break;
+				default:
+					System.out.println("Invalid input entered");
+				
+				}
 			
-			//getting user input
-			System.out.print("Please enter your choice : ");
-			option = sc.nextInt();
-			
-			//Performing user selected operation 
-			switch(option){
-			
-			case 1:
-				FileOperation.getAllFiles();
-				break;
-			case 2:
-				FileOperation.createFile();
-				break;
-			case 3:
-				FileOperation.deleteFile();
-				break;
-			case 4:
-				FileOperation.searchFile();
-				break;
-			case 5:
-				System.out.println("Application closed successfully.");
-				System.exit(0);
-				break;
-			default:
-				System.out.println("Invalid input entered");
-			
+			}catch(Exception e){
+				System.out.println("Invalid input. Please try again......");
+				
+				//assigning variable option value 6 to continue the execution of the applications
+				option = 6;
 			}
-			
-			System.out.println();
 			
 		}while(option>0);
 		
--- src/com/simplilearn/phase1/finalproject/FileOperation.java
+++ src/com/simplilearn/phase1/finalproject/FileOperation.java
@@ -1,15 +1,24 @@
 package com.simplilearn.phase1.finalproject;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class FileOperation {
 
 	//Declaring class variable
-	static final String projectFilesPath = "C:\\Users\\Jits\\Desktop\\Phase1_FinalProject_LockedMe\\Repository"; 
+	static final String projectFilesPath = "C:\\Users\\Jits\\Desktop\\Phase1_FinalProject_LockedMe\\Repository";
+	private static Scanner sc;
+	
+	/**
+	 * This method will retrieve all the available file and display to the end user.
+	 */
 	
 	public static void getAllFiles() {
 
-		//Declaring Vairable and Instantiate Object 
+		//Variable Declaration 
 		File folder = new File(projectFilesPath);
 		String[] listOfFiles = folder.list();
 				
@@ -27,21 +36,149 @@ public class FileOperation {
 		}else{
 			System.out.println("The folder is empty.");
 		}
-	}
-
-	public static void createFile() {
-		// TODO Auto-generated method stub
 		
 	}
+
+	/**
+	 * This method will read file details from user and create file in project folder.
+	 */
+	
+	public static void createFile() {
+
+		try{
+				
+			//Variable Declaration
+			sc = new Scanner(System.in);
+			String fileName;
+			int linesCount=0;
+			
+			//Getting filename from user
+			System.out.print("Enter file name : ");
+			fileName = sc.nextLine();
+			System.out.print("Enter how many lines in the file : ");
+			linesCount = Integer.parseInt(sc.nextLine());
+			
+			//Calling method to check user enter file extension or not
+			fileName = checkFileExtension(fileName);
+			
+			FileWriter fw = new FileWriter(projectFilesPath+"\\" + fileName);
+			
+			//Reading line by line from user
+			for(int i=1; i<=linesCount; i++){
+				System.out.println("Enter file line :");
+				fw.write(sc.nextLine()+"\n");
+			}
+			
+			System.out.println("File created successfully.");
+			fw.close();
+			
+		}catch (Exception e) {
+			System.out.println("Something went wrong. Try again....");
+		}
+	}
+
+	/**
+	 * This method will take filename from user and delete that file from project folder. 
+	 */
 
 	public static void deleteFile() {
-		// TODO Auto-generated method stub
+		
+		try{
+			//Variable Declaration
+			sc = new Scanner(System.in);
+			String fileName;
+			
+			//Getting filename from user
+			System.out.print("Enter (case sensitive) file name to be delete : ");
+			fileName = sc.nextLine();
+			
+			//Calling method to check user enter file extension or not
+			fileName = checkFileExtension(fileName);
+		
+			//Performing delete file action based on the file availability
+			if(checkFileExist(fileName)){
+				File file = new File(projectFilesPath+"\\" + fileName);
+				file.delete();
+				System.out.println("File deleted successfully.");
+			}else{
+				System.out.println("File does not exist.");
+			}
+			
+		}catch (Exception e) {
+			
+			System.out.println("Unable to delete the file. Please contact to admin@test.com");
+			//System.out.println(e);
+		}
 		
 	}
 
+	/**
+	 * This method will take filename from user and search file from project folder.
+	 */
 	public static void searchFile() {
-		// TODO Auto-generated method stub
 		
+		try{
+			
+			//Variable Declaration
+			sc = new Scanner(System.in);
+			String fileName = "";
+			
+			//Getting user input for searching file
+			System.out.print("Enter File name to search : ");
+			fileName = sc.nextLine();
+			
+			//Calling method to check user enter file extension or not
+			fileName = checkFileExtension(fileName);
+			
+			//Displaying the file availability status to user
+			if(checkFileExist(fileName)){	
+				System.out.println("File is available.");
+			}else{
+				System.out.println("File is not available.");
+			}	
+			
+		}catch(Exception e){
+			System.out.println("");
+		}
+		
+	}
+	
+	/**
+	 * This method will take file name as parameters and checks if
+	 * it is present in the project folder or not
+	 * @param fileName String
+	 * @return boolean
+	 */
+	private static boolean checkFileExist(String fileName){
+		
+		//Declaring variables
+		ArrayList<String> allAvailaableFilesName = new ArrayList<>();
+		File folder = new File(projectFilesPath);
+		String[] listOfFiles = folder.list();
+		
+		//Getting all available files from folder
+		if(listOfFiles.length>0){
+			for(String file : listOfFiles){
+				allAvailaableFilesName.add(file);
+			}	
+		}
+		return allAvailaableFilesName.contains(fileName);
+		
+	}
+		
+	/**
+	 * This method will take file name as parameter and check user has entered file extension or not
+	 * if not then it will add .txt at end of filename
+	 * @param fileName String
+	 * @return String
+	 */
+	private static String checkFileExtension(String fileName) {
+		
+		//Checking file type(.txt) entered or not
+		if(!fileName.contains(".txt")){
+			fileName = fileName+".txt";
+		}		
+		return fileName;
 	}
 
 }
